08f34704bb7e6a61eb2cd6ee54c65c7700f7b54d
Ethan Kisiel <ethan.a.kisiel@gmail.com>
2026-09-13 01:17:42 -0500

    add auto updater configuration

--- src/main/index.ts
+++ src/main/index.ts
@@ -3,6 +3,7 @@ import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
 import { Entry, EntrySearchFilter, SortConfig } from '../types/shared-types'
+import { autoUpdater } from 'electron-updater'
 
 type LedgerAddon = typeof import('*/ledger_addon.node');
 
@@ -120,6 +121,8 @@ app.whenReady().then(() => {
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
+
+  autoUpdater.checkForUpdatesAndNotify();
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
