dd511c43a068adeccff680581befc89f42f6b45a
Ethan Kisiel <ethan.a.kisiel@gmail.com>
2026-09-16 22:07:11 -0500

    update workflow, fix macos icon, etc

--- src/renderer/src/renderer.ts
+++ src/renderer/src/renderer.ts
@@ -218,7 +218,7 @@ export class AeraApp extends LitElement {
                         }
 
                         this._deletedEntry = null
-                        this.refreshTableData()
+                        await this.refreshTableData()
                         this._isDeleteModalShown = false
                       }
                     }
